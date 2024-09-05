021b72e2792d7c2d81e0989508d75fcb6612a97c
Denis <zaq.den@gmail.com>
2024-09-06 00:57:34 +0700

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,46 @@
+import time
+from time import sleep
+from threading import Thread
+
+# **Объявление функции write_words**
+def write_words(word_count, file_name):
+    with open(file_name, 'w') as file:
+        for i in range(1, word_count + 1):
+            file.write(f"Какое-то слово №{i}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+# **Взятие текущего времени**
+start_time = time.time()
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+# **Взятие текущего времени**
+end_time = time.time()
+
+# **Вывод разницы начала и конца работы функций**
+print(f"Работа потоков: {end_time - start_time:.2f} секунд")
+
+# **Взятие текущего времени**
+start_time = time.time()
+
+threads = []
+threads.append(Thread(target=write_words, args=(10, 'example5.txt')))
+threads.append(Thread(target=write_words, args=(30, 'example6.txt')))
+threads.append(Thread(target=write_words, args=(200, 'example7.txt')))
+threads.append(Thread(target=write_words, args=(100, 'example8.txt')))
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+# **Взятие текущего времени**
+end_time = time.time()
+
+# **Вывод разницы начала и конца работы потоков**
+print(f"Работа потоков: {end_time - start_time:.2f} секунд")
